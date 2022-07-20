f0cd0c1b71398bfbd0894e2fee7deec95c27edfc
Ahmed Alhor <ahor.c@tetco.sa>
2022-07-20 12:03:28 +0200

    init repo

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.listen(port, () => console.log(`app listening on http://localhost:${port}`));
