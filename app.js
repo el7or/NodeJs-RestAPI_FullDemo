54d785847bf85aff4660c6965fc382e3f9b82745
Ahmed Alhor <ahor.c@tetco.sa>
2022-07-22 19:03:30 +0200

    prepar to production

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger_output.json')
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
+const swaggerFile = require('./swagger_output.json');
 const authRoutes = require('./routes/auth');
 const rolesRoutes = require('./routes/roles');
 // const usersRoutes = require('./routes/users');
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.u9j79.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express();
 const port = 8080;
@@ -19,7 +25,16 @@ const fileStorage = multer.diskStorage({
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, uniqueSuffix + '-' + file.originalname)
     }
-})
+});
+
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'),
+    { flags: 'a' } //=> to append new logs to the file without delete old logs
+);
+
+app.use(helmet()); //=> add important headers to response (for production)
+app.use(compression()); //=> to minimize assets files (for production)
+app.use(morgan('combined', { stream: accessLogStream })); //=> for logging
 
 app.use(bodyParser.json()); // application/json
 app.use(multer({ storage: fileStorage }).single('image'));
@@ -47,10 +62,8 @@ app.use((error, req, res, next) => {
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerFile)); // => https://medium.com/swlh/automatic-api-documentation-in-node-js-using-swagger-dd1ab3c78284
 
 mongoose
-    .connect(
-        'mongodb+srv://node_test:node_test@cluster0.u9j79.mongodb.net/test-node?retryWrites=true&w=majority'
-    )
+    .connect(MONGODB_URI)
     .then(result => {
-        app.listen(port, () => console.log(`app listening on http://localhost:${port}`));
+        app.listen(process.env.PORT || port, () => console.log(`app listening on http://localhost:${port}`));
     })
     .catch(err => console.error(err));
