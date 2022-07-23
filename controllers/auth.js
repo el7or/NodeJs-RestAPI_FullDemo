8cbbf1e2e327a71c414e51afd2a99189040c3fee
Ahmed Alhor <ahor.c@tetco.sa>
2022-07-23 16:24:38 +0200

    unit testing

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,90 +10,83 @@ Date.prototype.addHours = function (h) {
     return this;
 }
 
-exports.login = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const error = new Error('Validation failed, entered data is incorrect.');
-        error.statusCode = 422;
-        error.data = errors.array();
-        throw error;
-    }
-    const name = req.body.name;
-    const password = req.body.password;
-    let loginUser;
-    User.findOne({ name: name })
-        .populate('roleId', 'name')
-        .then(user => {
-            if (!user) {
-                const error = new Error('A user with this name could not be found.');
-                error.statusCode = 401;
-                throw error;
-            }
-            loginUser = user;
-            return bcrypt.compare(password, user.password);
-        })
-        .then(isPasswordMatch => {
-            if (!isPasswordMatch) {
-                const error = new Error('Wrong password!');
-                error.statusCode = 401;
-                throw error;
-            }
-            const token = jwt.sign(
-                {
-                    name: loginUser.name,
-                    userId: loginUser._id.toString(),
-                    roleName: loginUser.roleId.name
-                },
-                'somesupersecretsecret',
-                { expiresIn: '1h' }
-            );
-            res.status(200).json({
-                token: token,
-                expiresIn: new Date().addHours(1)
-                //userId: loginUser._id.toString()
-            });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
+exports.login = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = new Error('Validation failed, entered data is incorrect.');
+            error.statusCode = 422;
+            error.data = errors.array();
+            throw error;
+        }
+        const name = req.body.name;
+        const password = req.body.password;
+        let user = await User.findOne({ name: name }).populate('roleId', 'name');
+        if (!user) {
+            const error = new Error('A user with this name could not be found.');
+            error.statusCode = 401;
+            throw error;
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            const error = new Error('Wrong password!');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = jwt.sign(
+            {
+                name: user.name,
+                userId: user._id.toString(),
+                roleName: user.roleId.name
+            },
+            'somesupersecretsecret',
+            { expiresIn: '1h' }
+        );
+        res.status(200).json({
+            token: token,
+            expiresIn: new Date().addHours(1)
+            //userId: loginUser._id.toString()
         });
+        return; // => need it only for unit test
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+        return err; // => need it only for unit test
+    }
 };
 
-exports.signup = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const error = new Error('Validation failed.');
-        error.statusCode = 422;
-        error.data = errors.array();
-        fileHelper.deleteFile(req.file?.path);
-        throw error;
-    }
-    const name = req.body.name;
-    const age = req.body.age;
-    const password = req.body.password;
-    const description = req.body.description;
-    const roleId = req.body.roleId;
-    const image = req.file;
-    bcrypt.hash(password, 12)
-        .then(hashedPassword => {
-            return User.create({
-                name: name,
-                password: hashedPassword,
-                age: age,
-                description: description,
-                roleId: roleId,
-                imageUrl: image?.path
-            });
-        })
-        .then(result => {
-            res.status(201).json({ message: 'User created!', userId: result._id });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
+exports.signup = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = new Error('Validation failed.');
+            error.statusCode = 422;
+            error.data = errors.array();
+            fileHelper.deleteFile(req.file?.path);
+            throw error;
+        }
+        const name = req.body.name;
+        const age = req.body.age;
+        const password = req.body.password;
+        const description = req.body.description;
+        const roleId = req.body.roleId;
+        const image = req.file;
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = await User.create({
+            name: name,
+            password: hashedPassword,
+            age: age,
+            description: description,
+            roleId: roleId,
+            imageUrl: image?.path
         });
+        res.status(201).json({ message: 'User created!', userId: user._id });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 };
--- controllers/roles.js
+++ controllers/roles.js
@@ -48,31 +48,30 @@ exports.getRole = (req, res, next) => {
     });
 };
 
-exports.postRole = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorrect.');
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
-  }
-  Role.create({
-    name: req.body.name,
-    description: req.body.description,
-    createdBy: req.userId
-  })
-    .then(result => {
-      res.status(201).json({
-        message: 'Role created successfully!',
-        result: result
-      });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+exports.postRole = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed, entered data is incorrect.');
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
+    }
+    const result = await Role.create({
+      name: req.body.name,
+      description: req.body.description,
+      createdBy: req.userId
     });
+    res.status(201).json({
+      message: 'Role created successfully!',
+      result: result
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.putRole = (req, res, next) => {
